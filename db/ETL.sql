565111c8c5ca9cd4c598a3625725bfdac2f59028
Kevin Yang <kyang9021@gmail.com>
2021-09-30 23:42:54 -0400

    updated schema, wrote and ran ETL.sql to add data to posgres

--- db/ETL.sql
+++ db/ETL.sql
@@ -1,6 +1,6 @@
 -- ONLY RUN THIS FILE AFTER schema.sql has been set-up in postgres
 
--- STEP #1: In command line, type psql catwalkoverview
+-- STEP #1: In command line, type psql catwalkreviews
 
 -- STEP #2: create 'data' directory in SDC directory; move csv data files into 'data'
 -- product.csv, features.csv, related.csv, styles.csv, skus.csv, photos.csv
@@ -11,3 +11,16 @@
 COPY reviews
 FROM '/Users/kevin-yang/work/Project-Atelier-Reviews/data/reviews.csv'
 WITH (FORMAT CSV, HEADER true, NULL 'null');
+
+COPY photos
+FROM '/Users/kevin-yang/work/Project-Atelier-Reviews/data/reviews_photos.csv'
+WITH (FORMAT CSV, HEADER true, NULL 'null');
+
+COPY characteristics
+FROM '/Users/kevin-yang/work/Project-Atelier-Reviews/data/characteristics.csv'
+WITH (FORMAT CSV, HEADER true, NULL 'null');
+
+COPY reviews_characteristics
+FROM '/Users/kevin-yang/work/Project-Atelier-Reviews/data/characteristic_reviews.csv'
+WITH (FORMAT CSV, HEADER true, NULL 'null');
+
--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,17 @@
-DROP DATABASE IF EXISTS catwalkReviews;
 CREATE DATABASE catwalkReviews;
 
 CREATE TABLE reviews(
   id SERIAL,
   product_id INTEGER NOT NULL,
   rating SMALLINT NOT NULL,
-  summary VARCHAR(60) DEFAULT NULL,
+  date BIGINT NOT NULL,
+  summary VARCHAR,
+  body VARCHAR(1000) NOT NULL,
   recommend BOOLEAN DEFAULT 'false',
   reported BOOLEAN DEFAULT 'false',
-  response VARCHAR(1000) DEFAULT NULL,
-  body VARCHAR(1000) NOT NULL,
-  date BIGINT NOT NULL,
   reviewer_name VARCHAR(60) NOT NULL,
   reviewer_email VARCHAR(60) NOT NULL,
+  response VARCHAR DEFAULT NULL,
   helpfulness SMALLINT NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
@@ -22,37 +21,31 @@ CREATE TABLE reviews(
 --
 -- ---
 
-DROP TABLE IF EXISTS photos;
-
 CREATE TABLE photos (
   id SERIAL UNIQUE,
-  review_id INTEGER
+  review_id INTEGER,
   url VARCHAR,
   PRIMARY KEY (id)
 );
-
-DROP TABLE IF EXISTS characteristics;
 
 CREATE TYPE features AS ENUM ('Size', 'Width', 'Comfort', 'Quality', 'Length', 'Fit');
 CREATE TABLE characteristics (
   id SERIAL PRIMARY KEY,
   product_id INTEGER,
   name features
-)
-
-DROP TABLE IF EXISTS reviews_characteristics;
+);
 
 CREATE TABLE reviews_characteristics (
   id SERIAL PRIMARY KEY,
   characteristic_id INTEGER,
   review_id INTEGER,
   value SMALLINT
-)
+);
 
 -- ---
 -- Foreign Keys
 -- ---
 
-ALTER TABLE photos ADD CONSTRAINT photos_review_id_fkey FOREIGN KEY (review_id) REFERENCES review(id);
+ALTER TABLE photos ADD CONSTRAINT photos_review_id_fkey FOREIGN KEY (review_id) REFERENCES reviews (id);
 ALTER TABLE reviews_characteristics ADD CONSTRAINT reviews_characteristics_characteristic_id_fkey FOREIGN KEY (characteristic_id) REFERENCES characteristics(id);
-ALTER TABLE reviews_characteristics ADD CONSTRAINT reviews_characteristics_characteristic_id_fkey FOREIGN KEY (review_id) REFERENCES review(id);
+ALTER TABLE reviews_characteristics ADD CONSTRAINT reviews_characteristics_characteristic_id_fkey FOREIGN KEY (review_id) REFERENCES reviews(id);
